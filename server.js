d12974913a652177c1a551ac9b12b953a2266b69
Manish <manish.xeno@gmail.com>
2024-08-25 22:32:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,20 +8,42 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
+// console.log(process.env.EMAIL_HOST_USER, process.env.EMAIL_HOST_PASSWORD)
+
+// const transporter = nodemailer.createTransport({
+//     host: "smpt.hostinger.com",
+//     secure: true, 
+//     secureConnection: false,
+//     tls: {
+//        ciphers: "SSLv3",
+//     },
+//     requireTLS: true,
+//     port: 465,
+//     debug: true,
+//     connectionTimeout: 10000,
+//     auth: {
+//         user: process.env.EMAIL_HOST_USER,
+//         pass: process.env.EMAIL_HOST_PASSWORD,
+//     },
+// });
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.titan.email",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_HOST_USER,
-        pass: process.env.EMAIL_HOST_PASSWORD
-    }
+        pass: process.env.EMAIL_HOST_PASSWORD,
+    },
 });
+
 
 app.post('/api/send-email', async (req, res) => {
     const { to, cc, bcc, subject, text } = req.body;
 
     try {
         await transporter.sendMail({
-            from: 'your-email@gmail.com',
+            from: process.env.EMAIL_HOST_USER,
             to,
             cc,
             bcc,
@@ -30,6 +52,8 @@ app.post('/api/send-email', async (req, res) => {
         });
         res.status(200).send('Email sent');
     } catch (error) {
+        // console.log(process.env.EMAIL_HOST_USER, process.env.EMAIL_HOST_PASSWORD)
+        console.log("this is the error", error)
         res.status(500).send('Error sending email');
     }
 });
